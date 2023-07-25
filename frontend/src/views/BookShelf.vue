8fce2ae718e4f071e11c2d7fb08c67a9a9bdbe1c
surfaceyu <surfaceyu@outlook.com>
2023-07-25 16:08:19 +0800

    1.新增小说文本搜索功能

--- frontend/src/views/BookShelf.vue
+++ frontend/src/views/BookShelf.vue
@@ -1,62 +1,109 @@
 <script lang="ts" setup>
 import { computed, provide, ref } from 'vue';
 import { type Ref } from 'vue';
-import { backend } from '@wailsjs/go/models';
-import DialogBookAdd from '@components/DialogBookAdd.vue'
+import { models } from '@wailsjs/go/models';
+import { Search } from '@element-plus/icons-vue'
+import { ElMessage } from 'element-plus'
+import { GetSites } from '@wailsjs/go/backend/BookSitesHandler';
+import { SearchBook, SearchChapter, SearchContent } from '@wailsjs/go/backend/BookHandler'
 
-const search = ref('')
-const tableData: Ref<backend.Book[]> = ref([]);
+const input3 = ref('')
+const select = ref(0)
+const selectBook = ref<models.Book>()
+const selectChapter = ref<models.Chapter>()
+const tableData: Ref<Array<models.BookSite>> = ref([]);
+const books = ref<Array<models.Book>>([]);
+const chapters = ref<Array<models.Chapter>>([]);
+const content = ref<models.Content>();
 
-const filterTableData = computed(() => {
-  return tableData.value.filter(data =>
-    !search.value || data.name.toLowerCase().includes(search.value.toLowerCase()))
+
+const initSitesData = async () => {
+    tableData.value = await GetSites()
+}
+const bookSites = computed(() => {
+    return tableData.value
 })
 
-const handleEdit = (index: number, row: backend.Book) => {
-  console.log(index, row);
-}
-const handleDelete = (index: number, row: backend.Book) => {
-  console.log(index, row);
+initSitesData()
+
+/**
+ * 搜索书籍
+ */
+ async function handleSearch(name: string, index: number) {
+    ElMessage.info('书本搜索中......')
+    // 清空现有的
+    books.value = []
+    books.value = await SearchBook(name, index)
+    selectBook.value = books.value[0];
+    ElMessage.success('书本搜索完成！')
 }
 
-const dialogFormVisible = ref(false);
+async function handleSelectBook(book: models.Book) {
+    ElMessage.info('章节搜索中......')
+    chapters.value = []
+    chapters.value = await SearchChapter(book, bookSites.value[select.value])
+    ElMessage.success('章节搜索完成！')
+}
 
-provide('dialogFormVisible', dialogFormVisible);
+async function handleSelectChapter(chapter: models.Chapter) {
+    ElMessage.info('正文搜索中......')
+    content.value = await SearchContent(chapter, bookSites.value[select.value])
+    ElMessage.success('正文搜索完成！')
+}
 </script>
 
 <template>
-  <h5 class="main-content-title">书架管理</h5>
-  <!-- 按钮组合 新增 -->
-  <!-- 书源列表 -->
-  <el-table :data="filterTableData" style="width: 100%" stripe>
-    <el-table-column label="名称" prop="name" width="150" />
-    <el-table-column label="作者" prop="author" width="100" />
-    <el-table-column label="介绍" prop="desc"  />
-    <el-table-column align="right" width="240">
-      <template #header>
-        <el-row>
-          <el-col align="left" :span="4">
-            <el-button size="small" @click="dialogFormVisible = true">新增</el-button>
-          </el-col>
-          <el-col :span="20">
-            <el-input v-model="search" size="small" placeholder="输入名称进行搜索" style="width: 160px;" />
-          </el-col>
-        </el-row>
-      </template>
-      <template #default="scope">
-        <el-button size="small" @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
-        <el-button size="small" type="danger" @click="handleDelete(scope.$index, scope.row)">删除</el-button>
-      </template>
-    </el-table-column>
-  </el-table>
-  <DialogBookAdd :data="tableData"/>
+    <h5 class="main-content-title">书架管理</h5>
+    <div class="mt-20">
+        <el-input v-model="input3" placeholder="输入书名进行搜索" class="input-with-select">
+            <template #append>
+                <el-button :icon="Search" @click="handleSearch(input3, select)" />
+            </template>
+            <template #prepend>
+                <el-select v-model="select" filterable placeholder="选择书源" style="width: 115px">
+                    <el-option v-for="(item, index) in bookSites" :label="item.name" :value="index" />
+                </el-select>
+            </template>
+        </el-input>
+    </div>
+    <el-row :gutter="20" class="mt-20">
+        <el-col :span="12" :offset="0">
+            <!-- 选择器选择选项后触发 handleSelectBook 方法-->
+            <el-select v-model="selectBook" value-key="bookName" placeholder="选择书本" @change="handleSelectBook">
+                <el-option v-for="item in books" :label="item.bookName" :value="item" :key="item.bookName" >
+                    <span style="float: left">{{ item.bookName }}</span>
+                    <span style="
+                  float: right;
+                  color: var(--el-text-color-secondary);
+                  font-size: 13px;
+                ">{{ item.author }}</span>
+                </el-option>
+            </el-select>
+        </el-col>
+        <el-col :span="12" :offset="0">
+            <el-select v-model="selectChapter" filterable value-key="chapterName" placeholder="选择章节"  @change="handleSelectChapter">
+                <el-option v-for="item in chapters" :label="item.chapterName" :value="item" :key="item.chapterName">
+                    {{ item.chapterName }}
+                </el-option>
+            </el-select>
+        </el-col>
+    </el-row>
+    <el-text>{{ content?.content }}</el-text>
 </template>
 
 <style scoped>
 .main-content-title {
-  text-align: left;
-  padding-left: 20px;
-  font-size: large;
-  color: #e7ac08;
+    text-align: left;
+    padding-left: 20px;
+    font-size: large;
+    color: #e7ac08;
+}
+
+.input-with-select .el-input-group__prepend {
+    background-color: var(--el-fill-color-blank);
+}
+
+.mt-20 {
+    margin-top: 20px;
 }
 </style>
