d6248d1789087d1503d99e29e3017b20826fc3c6
surfaceyu <surfaceyu@outlook.com>
2023-08-14 14:34:42 +0800

    1.新增 ai 辅助功能

--- frontend/src/router.ts
+++ frontend/src/router.ts
@@ -10,7 +10,7 @@ export const routes = [
     { path: '/', icon: Management, component: () => import('@views/BookShelf.vue') },
     { path: '/BookShelf', name: "书架管理", type: 'menu', icon: Management, component: () => import('@views/BookShelf.vue') },
     { path: '/BookSites', name: "书源管理", type: 'menu',  icon: Notebook, component: () => import('@views/BookSites.vue') },
-    { path: '/book2', name: "书籍制作", type: 'menu', icon: Notebook, component: () => import('@components/HelloWorld.vue') },
+    { path: '/book2', name: "AI  辅助", type: 'menu', icon: Notebook, component: () => import('@views/GPTTrd.vue') },
     { path: '/book3', name: "音频制作", type: 'menu', icon: Headset, component: () => import('@views/VoiceMaker.vue') },
     { path: '/book4', name: "音频管理", type: 'menu', icon: VideoPause, component: () => import('@components/HelloWorld.vue') },
 ]
---
+++ frontend/src/views/GPTTrd.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <h5 class="mb-2 h5-title">书籍制作</h5>
+  <iframe class="gptIf" src="https://c.binjie.fun/" frameborder="0"></iframe>
+</template>
+
+<style scoped>
+.h5-title {
+  text-align: left;
+  padding-left: 20px;
+  font-size: large;
+  color: #e7ac08;
+}
+.gptIf {
+  width: 100%;
+  height: 90%;
+}
+</style>
