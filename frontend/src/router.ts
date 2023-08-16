9f580bece579bf3e748399a9c659c21bd019b9b1
surfaceyu <surfaceyu@outlook.com>
2023-08-16 13:16:04 +0800

    1.修改文件名称

--- frontend/src/router.ts
+++ frontend/src/router.ts
@@ -9,7 +9,7 @@ import {
 export const routes = [
     { path: '/', icon: Management, component: () => import('@views/BookShelf.vue') },
     { path: '/BookShelf', name: "书架管理", type: 'menu', icon: Management, component: () => import('@views/BookShelf.vue') },
-    { path: '/BookSites', name: "书源管理", type: 'menu',  icon: Notebook, component: () => import('@views/BookSites.vue') },
+    { path: '/BookSites', name: "书源管理", type: 'menu',  icon: Notebook, component: () => import('@views/BookSitesRule.vue') },
     { path: '/book2', name: "AI  辅助", type: 'menu', icon: Notebook, component: () => import('@views/GPTTrd.vue') },
     { path: '/book3', name: "音频制作", type: 'menu', icon: Headset, component: () => import('@views/VoiceMaker.vue') },
     { path: '/book4', name: "音频管理", type: 'menu', icon: VideoPause, component: () => import('@components/HelloWorld.vue') },
